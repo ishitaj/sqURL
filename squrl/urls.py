2acbc116c873198988fb4213669bd77c8770c2c6
ijain89 <ijain89@gmail.com>
2017-08-13 20:29:46 +0530

    Making changes to urls.py corresponding to models

--- squrl/urls.py
+++ squrl/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^api', views.api, name='api'),
     url(r'^db', views.db, name='db'),
-    url(r'^(?P<slug>\w{7})$', views.redirect, name='redirect'),
+    url(r'^(?P<slug>[-\w]{4,50})$', views.redirect, name='redirect'),
 ]
 
 
