dabb30052b9acd8451bafcbb690cd8f9c32aa4f0
ijain89 <ijain89@gmail.com>
2017-08-13 20:28:37 +0530

    Updating models according to slug requirements

--- squrl/models.py
+++ squrl/models.py
@@ -1,15 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # URL model to create database to store the shortened urls
 class Squrls(models.Model):
     """
     Model for squrl url database
     """
-    squrl = models.SlugField(max_length=7,primary_key=True)
+    squrl = models.SlugField(max_length=50, primary_key=True)
     target = models.URLField(max_length=200)
     creation_date = models.DateTimeField(auto_now_add=True)
     access_date = models.DateTimeField(auto_now=True)
     visits = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.input_url
+        return self.target
+
+    def save(self, *args, **kwargs):
+        self.squrl = slugify(self.squrl)
+        super(Squrls, self).save(*args, **kwargs)
