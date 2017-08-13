54e33bb7bec9df04ba0a56dd1c3a0ab3f3656e16
ijain89 <ijain89@gmail.com>
2017-08-13 21:14:06 +0530

    Registering Squrls for editing in admin app

--- squrl/admin.py
+++ squrl/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Squrls
+
+# Register the Urls model with UrlsAdmin options to make it accessible in admin app
+
+class SqurlsAdmin(admin.ModelAdmin):
+    list_display = ('squrl','target', 'creation_date', 'access_date', 'visits')
+    ordering = ('-access_date',)
+
+admin.site.register(Squrls, SqurlsAdmin)
